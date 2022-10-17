1f576a5e7c5b4791589938f51ee7d552e1c31cd7
YolandiSwart <49681556+YolandiSwart@users.noreply.github.com>
2022-10-17 11:38:53 +0200

    Add files via upload

---
+++ split_into_chunks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#split expression into chunks
+for i in {1..22}; do split -d -l 50 TANDEM_GRCh38.chr${i}.expression "/mnt/lustre/users/yswart/eQTL/chunks/TANDEM_GRCh38.chr${i}.expression.";  done
+
+##Paste header in eveery file
+header=header_expressionfile.txt
+
+    for i in {1..22} ; do
+            for file in `ls /mnt/lustre/users/yswart/eQTL/chunks  | grep "chr${i}.expression"`
+            do
+                    paste ${header} > /mnt/lustre/users/yswart/eQTL/chunks/tmp.chr${i}.txt
+                    paste /mnt/lustre/users/yswart/eQTL/chunks/${file} >> /mnt/lustre/users/yswart/eQTL/chunks/tmp.chr${i}.txt
+                    rm /mnt/lustre/users/yswart/eQTL/chunks/${file}
+                    mv /mnt/lustre/users/yswart/eQTL/chunks/tmp.chr${i}.txt /mnt/lustre/users/yswart/eQTL/chunks/${file}
+            done
+    done
